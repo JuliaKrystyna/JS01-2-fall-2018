7c2a19ea8491eec198104abe06ba1ccf3506215a
JuliaKrystyna <juliakmalyszko@gmail.com>
2018-09-12 17:02:30 -0400

    Css not working but the button isssss

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $("button").click(() => {
-  sentence = `$(name) is $(age) years old.`
-  console.log(sentence)
+  let name = $("input#name").val();
+      let age = $("input#age").val();
+      console.log(`${name} is ${age} years old`);
 })
